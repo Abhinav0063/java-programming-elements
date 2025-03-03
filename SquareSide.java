eb2585b6235939d333b67faad18a09763a6707d8
Abhinav's MacBook Air M3 <psiecsaalry@gmail.com>
2025-03-03 15:28:34 +0530

    [Abhinav] ADD: SquareSide.java

---
+++ SquareSide.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SquareSide {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Take user input for perimeter
+        System.out.print("Enter the perimeter of the square: ");
+        double perimeter = input.nextDouble();
+
+        // Calculate the side of the square
+        double side = perimeter / 4;
+
+        System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+
+        input.close();
+    }
+}
