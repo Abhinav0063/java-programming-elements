588407722cc85bb7cae5aff939adc74fea29249a
Abhinav's MacBook Air M3 <psiecsaalry@gmail.com>
2025-03-03 15:34:10 +0530

    [Abhinav] ADD: QuotientRemainder.java

---
+++ QuotientRemainder.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Take user input for two numbers
+        System.out.print("Enter first number: ");
+        int number1 = input.nextInt();
+        System.out.print("Enter second number: ");
+        int number2 = input.nextInt();
+
+        // Compute quotient and remainder
+        int quotient = number1 / number2;
+        int remainder = number1 % number2;
+
+        System.out.println("The Quotient is " + quotient + " and the Remainder is " + remainder + " of two numbers " + number1 + " and " + number2);
+
+        input.close();
+    }
+}
